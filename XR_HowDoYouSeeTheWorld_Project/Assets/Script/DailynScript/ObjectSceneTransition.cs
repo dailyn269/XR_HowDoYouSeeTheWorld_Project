09743a63710745d9b59811e773dd316bd1479ef3
Dailyn Despradel <95663713+dailyn269@users.noreply.github.com>
2023-11-21 13:05:02 -0500

    dai

--- XR_HowDoYouSeeTheWorld_Project/Assets/Script/DailynScript/ObjectSceneTransition.cs
+++ XR_HowDoYouSeeTheWorld_Project/Assets/Script/DailynScript/ObjectSceneTransition.cs
@@ -3,41 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class ObjectSceneTransition : MonoBehaviour
 {
-    public string sceneToLoad;
-    private bool isGrabbed = false;
-
-    void Update()
-    {
-        CheckForGrabbing();
-    }
-
-    void CheckForGrabbing()
-    {
-        // Check for input from the Oculus controller to simulate grabbing
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && isGrabbed)
-        {
-            LoadNewScene();
-        }
-    }
+    public string sceneName;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("CatDoorKnob"))
+        if (other.CompareTag("Player"))
         {
-            isGrabbed = true;
+            SceneManager.LoadScene(sceneName);
         }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("CatDoorKnob"))
-        {
-            isGrabbed = false;
-        }
-    }
-
-    void LoadNewScene()
-    {
-        SceneManager.LoadScene(sceneToLoad);
     }
 }
